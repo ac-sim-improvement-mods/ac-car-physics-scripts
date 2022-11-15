fa774964c468a238b35619be35d915637766bfb6
William Gawlik <william.gawlik@outlook.com>
2022-11-14 22:13:43 -0800

    Fixed bmig

--- formula-1/data/bmig.lua
+++ formula-1/data/bmig.lua
@@ -8,6 +8,9 @@ local bmigMax = 0.09
 local bmigRamp = 0.30
 
 function brakemigration.control(data,dataCphys)
+    data.brake = 1
+    data.gas = 0
+
     if lastAState ~= car.extraA then
         if bmig == bmigMax then
             bmig = bmigMin
@@ -26,14 +29,21 @@ function brakemigration.control(data,dataCphys)
         lastBState = car.extraB
     end
 
-    local brakeBiasBase = car.brakeBias * 100
+    local brakeBiasBase = car.brakeBias
     local brakeBiasTotal = brakeBiasBase + ((math.clamp(data.brake-bmigRamp,0,1)/(1-bmigRamp)) * bmig * brakeBiasBase)
-    -- local bmigCalc = math.round((brakeBiasTotal - brakeBiasBase) / brakeBiasBase * 100,0)
+    local bmigCalc = math.round((brakeBiasTotal - brakeBiasBase) / brakeBiasBase * 100,0)
     local torqueFront = dataCphys.wheels[0].brakeTorque + dataCphys.wheels[1].brakeTorque
     local torqueRear = dataCphys.wheels[2].brakeTorque + dataCphys.wheels[3].brakeTorque
     local torqueTotal = torqueFront + torqueRear
-    local brakeBiasActual = torqueFront/torqueTotal*100
-    local bbdiff = (brakeBiasTotal*100)-brakeBiasActual
+    local brakeBiasActual = torqueFront/torqueTotal
+    local bbdiff = brakeBiasTotal-brakeBiasActual
+
+    ac.debug('bba',brakeBiasActual)
+    ac.debug('bmigCalc',bmigCalc)
+    ac.debug('bbdiff',bbdiff)
+    ac.debug('bbb',brakeBiasBase)
+    ac.debug('bbt',brakeBiasTotal)
+    ac.debug('torqueTotal',torqueTotal)
 
     data.controllerInputs[0] = brakeBiasTotal
     data.controllerInputs[1] = bmig * 100
